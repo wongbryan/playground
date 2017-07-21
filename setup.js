13da97af34bb9ee1e61d9f87a448754403931a7c
Bryan Wong <bryanwong@Bryans-MBP.hsd1.ca.comcast.net>
2017-07-21 13:24:28 -0700

    add gui

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-var camera, scene, renderer, controls;
+var camera, scene, renderer, controls, gui;
 var mouse = {x: 0, y: 0};
 
 var startTime = new Date().getTime();
@@ -6,10 +6,52 @@ var buffer = 100;
 var time;
 var nextUpdate = buffer;
 
+var parameters;
+
 function resize(){
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function updateGUI(){
+	shaderMat.uniforms['fSpeed'].value = parameters['speed'];
+	shaderMat.uniforms['fOpacity'].value = parameters['opacity'];
+	shaderMat.uniforms['amtRed'].value = parameters['red'];
+	shaderMat.uniforms['amtGreen'].value = parameters['green'];
+	shaderMat.uniforms['amtBlue'].value = parameters['blue'];
+}
+
+function changeToSphere(){
+	shaderPlane.geometry = new THREE.SphereBufferGeometry(1, 256, 256);
+}
+
+function changeToBox(){
+	shaderPlane.geometry = new THREE.BoxBufferGeometry(1, 1, 1, 256, 256);
+}
+
+function changeToCircle(){
+	shaderPlane.geometry = new THREE.CircleBufferGeometry(1, 256, 256);
+}
+
+function changeToCone(){
+	shaderPlane.geometry = new THREE.ConeBufferGeometry(1, .001, 256, 256);
+}
+
+function changeToPlane(){
+	shaderPlane.geometry = new THREE.PlaneBufferGeometry(1, 256, 256);
+}
+
+function changeToTorus(){
+	shaderPlane.geometry = new THREE.TorusBufferGeometry(1, .5, 256, 256);
+}
+
+function changeToKnot(){
+	shaderPlane.geometry = new THREE.TorusKnotBufferGeometry(1, .5, 256, 256);
+}
+
+function changeToRing(){
+	shaderPlane.geometry = new THREE.RingBufferGeometry(.5, 1, 256, 256);
 }
 
 function init() {
@@ -27,6 +69,44 @@ function init() {
 		controls.rotateSpeed = 2.0;
 		controls.panSpeed = 0.8;
 		controls.zoomSpeed = 1.5;
+
+		gui = new dat.GUI({
+			height: 6 * 32 - 1
+		});
+
+		parameters = {
+			speed: 50.0,
+			opacity: 0.05,
+			red: 1.0,
+			green: 1.0,
+			blue: 1.0,
+			sphere: changeToSphere,
+			cube: changeToBox,
+			circle: changeToCircle,
+			plane: changeToPlane,
+			cone: changeToCone,
+			torus: changeToTorus,
+			knot: changeToKnot,
+			ring: changeToRing
+		};
+
+		var properties = gui.addFolder('properties');
+
+		properties.add(parameters, 'speed').min(0.0).max(1000.0).step(1.0);
+		properties.add(parameters, 'opacity').min(0.0).max(1.0).step(0.01);
+		properties.add(parameters, 'red').min(0.0).max(1.0).step(0.01);
+		properties.add(parameters, 'green').min(0.0).max(1.0).step(0.01);
+		properties.add(parameters, 'blue').min(0.0).max(1.0).step(0.01);
+
+		var shape = gui.addFolder('shape');
+		shape.add(parameters, 'sphere');
+		shape.add(parameters, 'cube');
+		shape.add(parameters, 'circle');
+		shape.add(parameters, 'plane');
+		shape.add(parameters, 'cone');
+		shape.add(parameters, 'torus');
+		shape.add(parameters, 'knot');
+		shape.add(parameters, 'ring');
 
 		scene = new THREE.Scene();
 
@@ -48,14 +128,18 @@ function init() {
 			wireframe: true,
 			uniforms: {
 				"uTime": { type: "f", value: 0.0 },
-				"tDiffuse": { type: "t", value: noiseTex },
-				"uZoomMultiplyer": { type: "f", value: zoomDivider / (new THREE.Vector3().distanceTo(camera.position)) }
+				"tPerlin": { type: "t", value: noiseTex },
+				"fSpeed" : { type: "f", value: 50.0},
+				"fOpacity" : {type: "f", value: .05},
+				"amtRed" : {type: "f", value: 1.0},
+				"amtBlue" : {type: "f", value: 1.0},
+				"amtGreen" : {type: "f", value: 1.0}
 			},
 			depthTest: false,
 			vertexShader: document.getElementById( 'vertexShader' ).textContent,
 			fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 		} );
-		var shaderGeom = new THREE.SphereBufferGeometry(1, 256, 256);
+		var shaderGeom = new THREE.BoxBufferGeometry(1, 1, 1, 256, 256);
 
 		shaderPlane = new THREE.Mesh(shaderGeom, shaderMat);
 		shaderPlane.scale.set(5, 5, 5);
@@ -66,6 +150,7 @@ function init() {
 
 	function update(){
 		camera.lookAt(scene.position);
+		updateGUI();
 		controls.update();
 		
 		time = new Date().getTime() - startTime;
